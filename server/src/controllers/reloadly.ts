ab001020ef822998540b2ce20debf946c2058b15
iamenochchirima <iamenochchirima@gmail.com>
2024-09-03 17:26:41 +0200

    feat: completed top up apis

--- server/src/controllers/reloadly.ts
+++ server/src/controllers/reloadly.ts
@@ -13,6 +13,7 @@ export const airTimeDataTopUp = async (req: Request, res: Response) => {
     }
 
     if (!phoneNumber || !amount || !countryCode || !operatorId) {
+      console.log("phoneNumber", phoneNumber, "amount", amount, "countryCode", countryCode, "operatorId", operatorId);
       return res.status(400).json({ error: 'Missing required parameters' });
     }
 
@@ -33,7 +34,7 @@ export const airTimeDataTopUp = async (req: Request, res: Response) => {
     };
 
     const response = await axios.post(
-      'https://topups-sandbox+.reloadly.com/topups',
+      'https://topups-sandbox.reloadly.com/topups',
       payload,
       {
         headers: {
@@ -56,3 +57,110 @@ export const airTimeDataTopUp = async (req: Request, res: Response) => {
     }
   }
 };
+
+export const getNumberOperator = async (req: Request, res: Response) => {
+  try {
+    const { countryCode, phoneNumber, iso } = req.query;
+    const accessToken = req.cookies.reloadly_access_token;
+    
+    if (!accessToken) {
+      return res.status(401).json({ error: 'Access token not found' });
+    }
+
+    if (!countryCode) {
+      return res.status(400).json({ error: 'Missing required parameters' });
+    }
+
+    const response = await axios.get(
+      `https://topups-sandbox.reloadly.com/operators/auto-detect/phone/${phoneNumber}/countries/${iso}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error getting operators:', error);
+
+    if (error.response) {
+      res.status(error.response.status).json({ error: error.response.data.message });
+    } else if (error.request) {
+      res.status(500).json({ error: 'No response received from Reloadly API' });
+    } else {
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    }
+  }
+}
+
+export const getCountryOperators = async (req: Request, res: Response) => {
+  try {
+    const { countryCode } = req.query;
+    const accessToken = req.cookies.reloadly_access_token;
+    
+    if (!accessToken) {
+      return res.status(401).json({ error: 'Access token not found' });
+    }
+
+    if (!countryCode) {
+      return res.status(400).json({ error: 'Missing required parameters' });
+    }
+
+    const response = await axios.get(
+      `https://topups-sandbox.reloadly.com/operators/countries/${countryCode}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error getting operators:', error);
+
+    if (error.response) {
+      res.status(error.response.status).json({ error: error.response.data.message });
+    } else if (error.request) {
+      res.status(500).json({ error: 'No response received from Reloadly API' });
+    } else {
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    }
+  }
+}
+
+export const getAllOperators = async (req: Request, res: Response) => {
+  try {
+    const accessToken = req.cookies.reloadly_access_token;
+    
+    if (!accessToken) {
+      return res.status(401).json({ error: 'Access token not found' });
+    }
+
+    const response = await axios.get(
+      'https://topups-sandbox.reloadly.com/operators',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error getting operators:', error);
+
+    if (error.response) {
+      res.status(error.response.status).json({ error: error.response.data.message });
+    } else if (error.request) {
+      res.status(500).json({ error: 'No response received from Reloadly API' });
+    } else {
+      res.status(500).json({ error: 'An unexpected error occurred' });
+    }
+  }
+}
+
--- server/src/router/purchases.ts
+++ server/src/router/purchases.ts
@@ -1,7 +1,10 @@
 
-import { airTimeDataTopUp } from "../controllers/reloadly";
+import { airTimeDataTopUp, getAllOperators, getCountryOperators, getNumberOperator } from "../controllers/reloadly";
 import { Router } from "express";
 
 export default (router: Router) => {
   router.post("/buy/topup-airtime", airTimeDataTopUp);
+  router.get("/buy/get-operators", getAllOperators);
+  router.get("/buy/country-operators", getCountryOperators);
+  router.post("/buy/number-operator", getNumberOperator);
 };
