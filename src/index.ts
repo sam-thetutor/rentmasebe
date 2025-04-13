ffcad9ce3a676fc9835da7be68a4ef64d9ce4b69
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2025-04-13 23:54:32 +0300

    d

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 // CORS configuration
 app.use(
   cors({
-    origin: "*",
+    origin: '*',
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
     credentials: true,
   })
